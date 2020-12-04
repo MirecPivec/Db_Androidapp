94ec82f98bef41779a9d93a5e7ca14e979ca0ac2
Mirecpivec <mirecszabko@gmail.com>
2020-12-04 11:12:15 +0100

    Intial comit

---
+++ app/src/main/java/szabo/fpv/umb/ukladaniezaznamov/MainActivity.java
@@ -0,0 +1,37 @@
+package szabo.fpv.umb.ukladaniezaznamov;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+
+public class MainActivity extends AppCompatActivity {
+    ListView listView;
+    ArrayList<String> arrayList = new ArrayList<>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        listView = findViewById(R.id.listview);
+
+    //TODO test more data and chcek function work
+        ArrayAdapter arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1,addtoArrayfromDatabase());
+
+        listView.setAdapter(arrayAdapter);
+    }
+
+    private ArrayList<String> addtoArrayfromDatabase(){
+        //TODO Data from Database
+        arrayList.add("hello");
+        arrayList.add("okej");
+        arrayList.add("hello");
+        arrayList.add("okej");
+
+        return arrayList;
+    }
+}
